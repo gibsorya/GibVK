b07caad9422c65953e610448bdc38988bc63f52a
Ryan Gibson <gibsorya@gmail.com>
2021-08-23 19:16:14 -0500

    Removed all traces of Devices.cpp and .hpp

--- LearnVulkanNEW/Engine/Graphics.cpp
+++ LearnVulkanNEW/Engine/Graphics.cpp
@@ -22,7 +22,6 @@ namespace gibvk::graphics {
 		window = vulkan::create(WIDTH, HEIGHT, windowTitle);
 		instance = vulkan::createInstance();
 		debugMessenger = vulkan::debugutils::setupDebugMessenger();
-		//device = std::make_unique<gibvk::vulkan::devices::Devices>(true);
 		physicalDevice = vulkan::devices::pickPhysicalDevice();
 
 	}
@@ -71,15 +70,6 @@ namespace gibvk::graphics {
 
 		return *debugMessenger;
 	}
-
-	//const vulkan::devices::Devices& Graphics::getDevices() const
-	//{
-	//	if (device == nullptr) {
-	//		throw std::runtime_error("Device has not been initalized");
-	//	}
-
-	//	return *device;
-	//}
 
 	const vulkan::devices::PhysicalDevices& Graphics::getPhysicalDevice() const
 	{
